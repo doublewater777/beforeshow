0aed191781ef00a9690a1fd45b0f02141c4ba67c
Doublewater777 <return_panyang@163.com>
2026-09-03 15:43:18 +0800

    feat: add opening artist tier model

---
+++ apps/ios/BeforeShow/Domain/Listening/ShowOpeningArtistTier.swift
@@ -0,0 +1,38 @@
+import Foundation
+import SwiftData
+
+@Model
+final class ShowOpeningArtistTier {
+    @Attribute(.unique)
+    private(set) var uniqueKey: String
+    private(set) var showID: UUID
+    private(set) var artistID: String
+    var artistNameAtCapture: String
+    var tierRawValue: String
+    var baselineCapturedAt: Date
+    var catalogSnapshotFetchedAt: Date
+    var resolvedAt: Date
+
+    init(
+        showID: UUID,
+        artistID: String,
+        artistNameAtCapture: String,
+        tierRawValue: String,
+        baselineCapturedAt: Date,
+        catalogSnapshotFetchedAt: Date,
+        resolvedAt: Date = Date()
+    ) {
+        self.uniqueKey = Self.makeUniqueKey(showID: showID, artistID: artistID)
+        self.showID = showID
+        self.artistID = artistID
+        self.artistNameAtCapture = artistNameAtCapture
+        self.tierRawValue = tierRawValue
+        self.baselineCapturedAt = baselineCapturedAt
+        self.catalogSnapshotFetchedAt = catalogSnapshotFetchedAt
+        self.resolvedAt = resolvedAt
+    }
+
+    static func makeUniqueKey(showID: UUID, artistID: String) -> String {
+        "\(showID.uuidString)|\(artistID)"
+    }
+}
