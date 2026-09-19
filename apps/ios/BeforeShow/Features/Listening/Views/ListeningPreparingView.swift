b2e49322a9ab6ce1689fd62f39615be91fcadcc3
Doublewater777 <return_panyang@163.com>
2026-09-19 19:36:10 +0800

    Align listening preparing spacing

--- apps/ios/BeforeShow/Features/Listening/Views/ListeningPreparingView.swift
+++ apps/ios/BeforeShow/Features/Listening/Views/ListeningPreparingView.swift
@@ -19,7 +19,7 @@ struct ListeningPreparingView: View {
             VStack(spacing: 0) {
                 ListeningRoomHeader(mode: .connecting)
                 ScrollView(showsIndicators: false) {
-                    VStack(spacing: BSSpacing.sm) {
+                    VStack(spacing: BSSpacing.xs) {
                         if !artists.isEmpty {
                             ListeningArtistSelector(artists: artists, selection: .all, select: { _ in }, onConnect: { _, _ in })
                         }
