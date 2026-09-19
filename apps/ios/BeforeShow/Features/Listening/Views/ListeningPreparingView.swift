cd8c2c172771142c1e401dfbd97d739c317651d8
Doublewater777 <return_panyang@163.com>
2026-09-19 20:12:56 +0800

    Restore Listen preparing tab clearance

--- apps/ios/BeforeShow/Features/Listening/Views/ListeningPreparingView.swift
+++ apps/ios/BeforeShow/Features/Listening/Views/ListeningPreparingView.swift
@@ -38,6 +38,7 @@ struct ListeningPreparingView: View {
                     .coordinateSpace(name: "listeningContent")
                     .padding(.horizontal, BSSpacing.roomy)
                     .padding(.top, BSSpacing.sm)
+                    .padding(.bottom, BSLayout.tabBarContentInset)
                 }
                 .scrollDisabled(true)
             }
