222fc8641de2930fedb648810e3a5e39e2b71a48
Doublewater777 <return_panyang@163.com>
2026-09-19 17:00:19 +0800

    Remove obsolete root bottom padding

--- apps/ios/BeforeShow/Features/Footprints/FootprintsView.swift
+++ apps/ios/BeforeShow/Features/Footprints/FootprintsView.swift
@@ -163,7 +163,6 @@ private struct FootprintPreparingView: View {
                         .padding(.horizontal, 20)
                         .padding(.top, 16)
                 }
-                .padding(.bottom, 0)
             }
             .scrollDisabled(true)
             .scrollIndicators(.hidden)
