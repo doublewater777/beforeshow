8f6b6f6cfe66b5a5c8f90f1b94408f5426fe320f
Doublewater777 <return_panyang@163.com>
2026-09-21 10:24:06 +0800

    Warm only the remaining disc texture

--- apps/ios/BeforeShow/Features/Listening/Player/ListeningPlayerWarmup.swift
+++ apps/ios/BeforeShow/Features/Listening/Player/ListeningPlayerWarmup.swift
@@ -11,14 +11,7 @@ import UIKit
         Task(priority: .utility) { @MainActor in
             CDSoundPlayer.shared.warmup()
             #if canImport(UIKit)
-            for name in [
-                CDPlayerConfiguration.standard.assets.body,
-                CDPlayerConfiguration.standard.assets.lidOuter,
-                CDPlayerConfiguration.standard.assets.lidInner,
-                CDPlayerConfiguration.standard.assets.disc
-            ] {
-                _ = UIImage(named: name)
-            }
+            _ = UIImage(named: CDPlayerConfiguration.standard.assets.disc)
             #endif
         }
     }
